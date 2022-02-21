ff4699516cf58395c3bb8f0c26c9fab2604a8ff3
Yura Soboliev <yuraslbv.y@gmail.com>
2022-02-21 14:47:30 +0200

    hangman ready

--- python-basics/basic_operators.py
+++ python-basics/basic_operators.py
@@ -1,7 +1,7 @@
 x = object()
 y = object()
 
-x_list = [0,1,2,3,4,5,6,7,8,9]
+x_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 y_list = ["A", "B", "C", "D", "E", "F", "G", "J", "K", "L"]
 big_list = x_list + y_list
 
--- python-basics/files.py
+++ python-basics/files.py
@@ -1,9 +1,9 @@
-with open('test.txt', 'w') as f:
-    f.write('Hello world\n')
-    f.write('And you\n')
+with open("test.txt", "w") as f:
+    f.write("Hello world\n")
+    f.write("And you\n")
 
-with open('test.txt', 'a') as f:
-    f.write("What\'s up?")
+with open("test.txt", "a") as f:
+    f.write("What's up?")
 
-with open('test.txt', 'r') as f:
+with open("test.txt", "r") as f:
     print(f.read())
